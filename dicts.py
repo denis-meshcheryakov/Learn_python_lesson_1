85ca65c947e3ddf7c5355ebd7ab0b26f18017c86
ktulhoo86 <ktulhoo.pyneng@yandex.ru>
2021-09-08 21:24:57 +0300

    change dicts.py

--- dicts.py
+++ dicts.py
@@ -2,7 +2,9 @@ dict1 = {"city": "Москва", "temperature": "20"}
 print(dict1['city'])
 dict1['temperature'] = int(dict1['temperature']) - 5
 print(dict1)
-print(dict1.get('country'))
+if 'country' not in dict1:
+    print('country is not in keys')
 print(dict1.get('country', 'Россия'))
 dict1['date'] = '27.05.2019'
 print(len(dict1))
+print(dict1)
